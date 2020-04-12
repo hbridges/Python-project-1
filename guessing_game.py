08e8edb7d711ddf2cc2197ec542a33157af9663c
Harvey Bridges <hdbridges11@gmail.com>
2020-04-11 20:21:07 -0600

    basic working game added

--- guessing_game.py
+++ guessing_game.py
@@ -3,7 +3,7 @@ Python Web Development Techdegree
 Project 1 - Number Guessing Game
 --------------------------------
 
-For this first project we will be using Workspaces. 
+For this first project we will be using Workspaces.
 
 NOTE: If you strongly prefer to work locally on your own computer, you can totally do that by clicking: File -> Download Workspace in the file menu after you fork the snapshot of this workspace.
 
@@ -13,25 +13,30 @@ import random
 
 
 def start_game():
+    print("Welcome to the guessing game good luck!")
+    answer = random.randrange(11)
+    count = 1
+
+    while True:
+        guess = input("Pick a number between 1 and 10  ")
+
+        if int(guess) > answer:
+            print("It's lower. ")
+            count += 1
+            continue
+        if int(guess) < answer:
+            print("It's higher.")
+            count += 1
+            continue
+        if int(guess) == answer:
+            print("Thats the number you won! It took you {} attempts".format(count))
+            print("THE GAME IS OVER!")
+            break
+
     """Psuedo-code Hints
-    
-    When the program starts, we want to:
-    ------------------------------------
-    1. Display an intro/welcome message to the player.
-    2. Store a random number as the answer/solution.
-    3. Continuously prompt the player for a guess.
-      a. If the guess greater than the solution, display to the player "It's lower".
-      b. If the guess is less than the solution, display to the player "It's higher".
-    
-    4. Once the guess is correct, stop looping, inform the user they "Got it"
-         and show how many attempts it took them to get the correct number.
-    5. Let the player know the game is ending, or something that indicates the game is over.
-    
     ( You can add more features/enhancements if you'd like to. )
     """
-    # write your code inside this function.
 
 
 if __name__ == '__main__':
-    # Kick off the program by calling the start_game function.
     start_game()
